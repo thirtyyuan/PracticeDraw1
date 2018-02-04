f3d781aa663a983497c90cc6a12f61797599383e
thirtyyuan <thirtyyuan@gmail.com>
2018-02-04 10:14:56 +0800

    init 10

--- app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice10HistogramView.kt
+++ app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice10HistogramView.kt
@@ -2,6 +2,8 @@ package com.hencoder.hencoderpracticedraw1.practice
 
 import android.content.Context
 import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
 import android.util.AttributeSet
 import android.view.View
 
@@ -18,5 +20,16 @@ class Practice10HistogramView : View {
 
         //        综合练习
         //        练习内容：使用各种 Canvas.drawXXX() 方法画直方图
+        val mPaint = Paint()
+        mPaint.flags = Paint.ANTI_ALIAS_FLAG
+        mPaint.strokeWidth = 3F
+        mPaint.color = Color.WHITE
+        val originX = 100F
+        val originY = 500F
+        val xAxis = 700F
+        val yAxis = 100F
+        val columnWidth = 100F
+        canvas.drawLine(originX, 20F, originX, originY, mPaint)
+        canvas.drawLine(originX, originY, 1000F,originY, mPaint)
     }
 }
